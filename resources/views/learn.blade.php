e1420d64f8f9c9bf52e3158c7baf5b7e359ba451
Samuel Roland <samuelroland@noreply.codeberg.org>
2022-02-11 00:47:08 +0100

    learn: create fake content and basic design of the page with articles menu and content

--- resources/views/learn.blade.php
+++ resources/views/learn.blade.php
@@ -1,1 +1,36 @@
-<x-layout> This is the Learn page </x-layout>
+<x-layout>
+    <div class="flex w-full space-x-5">
+        <div class="w-96 min-w-max border-r border-blue-400 pr-3">
+            <div>
+                <div>Introduction</div>
+                <div class="ml-4">
+                    <div>About</div>
+                    <div>Contribute</div>
+                    <div>Disclaimer</div>
+                </div>
+                <div>Strategy</div>
+                <div class="ml-4">
+                    <div>Authors</div>
+                    <div>Regional strategy</div>
+                    <div>Short-term strategy</div>
+                </div>
+            </div>
+
+        </div>
+
+        <div>
+            <h1>Introduction</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Delectus, sunt quod animi sit harum unde similique soluta dolorum ut quaerat!
+            <h2>A great article</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Delectus, sunt quod animi sit harum unde similique soluta dolorum ut quaerat!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Delectus, sunt quod animi sit harum unde similique soluta dolorum ut quaerat!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Delectus, sunt quod animi sit harum unde similique soluta dolorum ut quaerat!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Delectus, sunt quod animi sit harum unde similique soluta dolorum ut quaerat!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Delectus, sunt quod animi sit harum unde similique soluta dolorum ut quaerat!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Delectus, sunt quod animi sit harum unde similique soluta dolorum ut quaerat!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nihil labore exercitationem amet laborum nulla, impedit id repellendus. Delectus, sunt quod animi sit harum unde similique soluta dolorum ut quaerat!</p>
+
+
+        </div>
+    </div>
+</x-layout>
