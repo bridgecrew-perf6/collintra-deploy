fb7ce7bc9cc00e5c2d3c417c239c340ce2ea76da
Samuel Roland <samuelroland@noreply.codeberg.org>
2022-02-11 01:01:12 +0100

    Article: create migration, model, seeder and factory for articles

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    use WithFaker;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => Str::ucfirst($this->faker->word()),
+            'body' => "<p>" . implode("</p>
+            <p>", $this->faker->paragraphs(15)) . "</p>",
+        ];
+    }
+}
