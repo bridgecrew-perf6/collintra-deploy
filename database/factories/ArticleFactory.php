e3998cc170db9247ae0e047bf77041fe6f0fa0a8
Samuel Roland <samuelroland@noreply.codeberg.org>
2022-02-12 00:02:52 +0100

    ArticleFactory: enhance title and body generation

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -25,21 +25,29 @@ class ArticleFactory extends Factory
 
             //Heading and then texts
             $headingLevel = rand(2, 5);
-            $body .= "<h" . $headingLevel . ">" . $this->faker->sentence(rand(5, 8)) . "</h" . $headingLevel . ">";
+            $body .= "<h" . $headingLevel . ">" . $this->removeEndingChar($this->faker->sentence(rand(5, 8))) . "</h" . $headingLevel . ">";
             $body .= "<p>" . $this->faker->text(rand(200, 800)) . "</p>";
             $body .= "<p>" . $this->faker->text(rand(300, 600)) . "</p>";
 
             //Heading and then texts
             $headingLevel = rand(2, 5);
-            $body .= "<h" . $headingLevel . ">" . $this->faker->sentence(rand(2, 8)) . "</h" . $headingLevel . ">";
+            $body .= "<h" . $headingLevel . ">" . $this->removeEndingChar($this->faker->sentence(rand(2, 8))) . "</h" . $headingLevel . ">";
             $body .= "<p>" . $this->faker->text(rand(200, 1500)) . "</p>";
             $body .= "<p>" . $this->faker->text(rand(100, 200)) . "</p>";
         }
 
+        $title = Str::ucfirst($this->faker->words(rand(1, 6), true));
+
         return [
-            'title' => Str::ucfirst($this->faker->words(rand(1, 6), true)),
+            'title' => $title,
             'body' => $body,
             'article_id' => null
         ];
+    }
+
+    protected function removeEndingChar($string)
+    {
+        return substr($string, 0, strlen($string) - 1); //Remove the dot at the end
+
     }
 }
