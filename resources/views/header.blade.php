894bc1f8660e79e33b80f9fded0c031947158f41
Samuel Roland <samuelroland@noreply.codeberg.org>
2022-02-12 01:21:51 +0100

    header: integrate the new logo and version number

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,9 @@
 <header class="flex border-b mb-3 border-blue-400">
-    <div class="m-2 rounded-full border border-blue-400 h-14 w-14 text-center">LOGO</div>
+    <div class="h-8 mt-2 mb-4 ml-1 w-full flex items-end">
+        <img src="/logo.png" alt="Collintra" class="h-6 inline" />
+        <div class="text-gray-600 italic text-sm w-min inline-flex flex-col justify-end ml-1 mr-1 translate-y-3">v1.0</div>
+    </div>
+    <div class="m-2 rounded-full border border-blue-400 h-14 w-14 text-center" hidden></div>
     <div class="flex-1 justify-end mr-5 flex items-center space-x-3">
         @livewire("search")
         <x-menu.button route="/">Home</x-menu.button>
