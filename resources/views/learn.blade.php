7d19ad0e33ffde60f58398179de2d0d735a9dbec
Samuel Roland <samuelroland@noreply.codeberg.org>
2022-03-06 02:39:38 +0100

    learn: almost working responsive version of the articles menu and the learn page

--- resources/views/learn.blade.php
+++ resources/views/learn.blade.php
@@ -2,9 +2,9 @@
 use Carbon\Carbon;
 @endphp
 <x-layout>
-    <div class="flex w-full space-x-5">
-        <div class="border-r border-blue-400 pr-3 p-1" style="width: 250px;">
-            <div>
+    <div class="md:flex relative">
+        <div class="md:border-r border md:border-0 rounded-md md:rounded-none w-full md:w-[250px] border-blue-800 pr-3 p-1 md:relative absolute z-40 top-0 bg-bluebg max-h-screen overflow-y-scroll overflow-x-hidden shadow-xl md:shadow-none" @click.away="window.innerWidth <= 768 ? menuOpen = false : null" x-show="window.innerWidth > 768 || menuOpen" x-collapse x-cloak="mobile" @resize.window="window.innerWidth <= 768 ? menuOpen = false : null">
+            <div class="max-h-64 w-full">
                 @foreach($articles as $article)
                 @if($article->article_id == null)
                 <div class="font-semibold text-lg h-6 overflow-hidden text-ellipsis whitespace-nowrap {{ $article->is($currentArticle) ? 'selected' : ''}}"><a href="{{ $article->path() }}">{{ $article->title }}</a></div>
@@ -22,11 +22,11 @@ use Carbon\Carbon;
             </div>
         </div>
 
-        <div class="">
-            <div class="prose prose-slate">
-                <div class="flex items-center">
+        <div class="max-w-full flex-1 mt-3 md:mt-5">
+            <div class="prose prose-slate px-0 md:px-10 lg:px-24 max-w-full overflow-hidden">
+                <div class="flex flex-wrap items-center mb-3 w-full md:mb-5">
 
-                    <h1 class="not-prose flex-1 !my-0">{{ $currentArticle->title }}</h1>
+                    <h1 class="not-prose flex-1 min-w-max !my-0 mr-2">{{ $currentArticle->title }}</h1>
 
                     <div class="not-prose text-gray-500 text-sm italic">
                         <span>Last update: </span>
@@ -39,8 +39,11 @@ use Carbon\Carbon;
                     </div>
                 </div>
 
-                <div>{!! $currentArticle->body !!}</div>
+                <div class="">{!! $currentArticle->body !!}</div>
             </div>
         </div>
     </div>
+    <script>
+
+    </script>
 </x-layout>
